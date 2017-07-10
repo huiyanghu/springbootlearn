66e4837a191082daa1899b964da1dd7d5d268dbb
hhy <hhy>
2017-07-10 11:56:54 +0800

    test

---
+++ src/main/java/com/hhy/springbootlearn/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.hhy.springbootlearn.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by hhy on 17/7/10.
+ */
+@Controller
+public class TestController {
+}
